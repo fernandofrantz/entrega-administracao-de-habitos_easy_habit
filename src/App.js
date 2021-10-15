7036754260e3906bf2ace51010b1f0bf80487501
lourivanrluz <lourivan-rodrigues@hotmail.com>
2021-10-15 16:47:36 -0300

    feat/ formulario ainda em progresso

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import FormGoal from "./Components/FormGoal";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <FormGoal id={11} type={"register"} />
       </header>
     </div>
   );
--- src/Components/FormGoal/index.js
+++ src/Components/FormGoal/index.js
@@ -1,8 +1,54 @@
-import * as yup from "yup";
-import { useHistory } from "react-router";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useState } from "react";
 
-const FormGoal = ({ id, type }) => {
-  return <form>{"title,difficulty,how_much_achieved e o id do group"}</form>;
+const FormGoal = ({ idGroup, idGoal, type }) => {
+  const [title, setTitle] = useState("");
+  const [dificult, setDificult] = useState("");
+
+  const createGoal = () => {
+    const token = JSON.parse(localStorage.getItem("@EH")) || "";
+    const data = {
+      title: title,
+      difficulty: dificult,
+      group: idGroup,
+    };
+
+    axios
+      .post(`https://kenzie-habits.herokuapp.com/goals/${idGoal}`, data, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => console.log("criou"))
+      .catch((err) => console.log("nao criou"));
+  };
+
+  const editeGoal = () => {
+    const data = {};
+    if (title) data.title = title;
+    if (dificult) data.difficulty = dificult;
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <input
+        placeholder="Titulo da Meta"
+        type="text"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+        required={type === "register" ? true : false}
+      />
+      <input
+        placeholder="Dificuldade"
+        type="text"
+        value={dificult}
+        onChange={(event) => setDificult(event.target.value)}
+        required={type === "register" ? true : false}
+      />
+      <button onClick={type === "register" ? createGoal : editeGoal}>
+        {type === "register" ? "Cadastrar" : "Editar"}
+      </button>
+    </div>
+  );
 };
+
+export default FormGoal;
