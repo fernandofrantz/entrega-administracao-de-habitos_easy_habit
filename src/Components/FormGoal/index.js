eb85b0e76da63f3b612d4b1644b949f4e18c965c
lourivanrluz <lourivan-rodrigues@hotmail.com>
2021-10-15 15:13:47 -0300

    feat/ implementaçao do formulario de goal ainda em progresso

---
+++ src/Components/FormGoal/index.js
@@ -0,0 +1,8 @@
+import * as yup from "yup";
+import { useHistory } from "react-router";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+
+const FormGoal = ({ id, type }) => {
+  return <form>{"title,difficulty,how_much_achieved e o id do group"}</form>;
+};
