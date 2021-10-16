7e1525e2fdde5dabc2972a832a36727a7291e8f3
lourivanrluz <lourivan-rodrigues@hotmail.com>
2021-10-16 10:39:08 -0300

    refactore/ mudanças na funçao de login

--- src/Providers/User/index.js
+++ src/Providers/User/index.js
@@ -12,12 +12,34 @@ export const UserProvider = ({ children }) => {
     }
   };
 
+  /* 
+    const token = JSON.parse(localStorage.getItem("@ADM_HABIT:token"))||'';
+    if (token) {
+      setAuth(true);
+    };
+  */
+
+  /* 
+  const Login = (data) => {
+    console.log(data);
+    api
+      .post("/sessions/", data)
+      .then((response) => {
+        const token = response.data.access;
+        localStorage.setItem("@EH", JSON.stringify(token));
+        setAuth(true);
+        history.push("/");
+      })
+      .catch(() => {
+        toast.error("usuário não cadastrado ou senha inválida");
+      });
+  };  
+  */
+
   const Logout = () => {
     setAuth(false);
     localStorage.clear();
   };
-
-
 
   return (
     <UserContext.Provider value={{ Login, Logout, auth }}>
