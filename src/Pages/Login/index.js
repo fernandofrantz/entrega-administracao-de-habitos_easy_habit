1cd7fdd58ba1d4d1d11e028e76afcb9df3127f63
IqueMoraes <iquedemoraes@hotmail.com>
2021-10-18 18:14:52 -0300

    apos merge com parcial_development

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,4 +1,6 @@
 import { FormLogin } from "../../Components/FormLogin";
+import { useUser } from "../../Providers/User";
+import { useHistory } from "react-router-dom";
 
 export const Login = () => {
   const history = useHistory();
