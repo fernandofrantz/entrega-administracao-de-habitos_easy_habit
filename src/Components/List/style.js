a6542ef19735fe00d05d79994ba2ce933cb8512f
fernandofrantz <fernandofrantz12@gmail.com>
2021-10-20 15:15:12 -0300

    fix/parte do código que era necessária estava comentada

--- src/Components/List/style.js
+++ src/Components/List/style.js
@@ -1,16 +1,14 @@
-// import styled from "styled-components";
-// export const UlStyled = styled.ul`
-//   width: 270px;
-//   list-style: none;
+import styled from "styled-components";
+export const UlStyled = styled.ul`
+  width: 90vw;
+  list-style: none;
 
-//   display: flex;
-//   flex-wrap: wrap;
-//   flex-direction: column;
-//   align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
 
-//   li {
-//     /* margin: 15px 15px; */
-//     width: 240px;
-    
-//   }
-// `;
+  li {
+    margin: 15px 15px;
+    width: 240px;
+  }
+`;
