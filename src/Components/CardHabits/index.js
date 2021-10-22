7a46ef6fcb40b682b6915bd5f2fd7dda648f1abd
IqueMoraes <iquedemoraes@hotmail.com>
2021-10-21 21:12:38 -0300

    fix/ajustes de codigos

--- src/Components/CardHabits/index.js
+++ src/Components/CardHabits/index.js
@@ -3,8 +3,8 @@ import { useHabits } from "../../Providers/Habits";
 import { MdModeEdit, MdDelete } from "react-icons/md";
 import { ButtonsBox, CategoryColor, Container } from "./styles";
 // import { FormCreateHabits } from "../FormCreateHabits";
-import { FormHabitsEdit } from "../EditHabts/Index";
 import FormEditHabits from "../EditHabits";
+
 
 export const CardHabits = ({ item, editable, backGroundColor }) => {
   const [showForm, setShowForm] = useState(false);
@@ -14,7 +14,7 @@ export const CardHabits = ({ item, editable, backGroundColor }) => {
     <Container>
       <CategoryColor backGroundColor={backGroundColor} />
       <h2 done={done}>{item.title}</h2>
-      {showForm && <FormHabitsEdit type={"edit"} idHabit={item.id} setShowForm={setShowForm}/>}
+      {showForm && <FormEditHabits type={"edit"} idHabit={item.id} setShowForm={setShowForm}/>}
       {editable ? (
         <ButtonsBox>
           <MdModeEdit onClick={() => setShowForm(!showForm)} />
